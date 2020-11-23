d36babc0f1828993f0c95a1dd5ae8450660758c7
Nikolai Mavrenkov <koluch@koluch.ru>
2020-11-23 05:08:21 +0300

    Try to fix issue with dark-mode icon

--- app/assets/bg.js
+++ app/assets/bg.js
@@ -1,12 +1,13 @@
-browser.runtime.onInstalled.addListener(() => {
-    const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
-    function setIcon(isDark) {
-        browser.browserAction.setIcon({
-            path: isDark ? "/icons/icon-dark.svg" : "/icons/icon-light.svg"
-        });
-    }
-    mediaQueryList.addEventListener("change", e => {
-        setIcon(e.matches);
-    });
-    setIcon(mediaQueryList.matches);
-});
+const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+
+function updateIcon() {
+  const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  browser.browserAction.setIcon({
+    path: isDark ? "/icons/icon-dark.svg" : "/icons/icon-light.svg"
+  });
+}
+
+mediaQueryList.addEventListener("change", updateIcon);
+browser.runtime.onInstalled.addListener(updateIcon);
+browser.theme.onUpdated.addListener(updateIcon);
+updateIcon();
